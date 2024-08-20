2037624ebe11844e7dc4b81af5e5b7fbd3007afc
GyeongHwa <gyeonghwaa_@naver.com>
2024-08-20 23:37:51 +0900

    4강 columns 이용한 위젯 배치

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from typeArr import type_emoji_dict
+from config import type_emoji_dict, pokemons
 
 st.set_page_config(
     page_title="포켓몬 도감",
@@ -8,16 +8,19 @@ st.set_page_config(
 
 st.title("streamlit 포켓몬 도감")
 st.markdown("**포켓몬**을 하나씩 추가해서 도감을 채워보세요")
-# 속성추가
 
-pokemon={
-    "name":"누오",
-    "types":["물","땅"],
-    "image_url":"https://i.namu.wiki/i/e-W40ZAdGkzTIHMZmWcWNVy1FD6R2Wbt45XAQvpmwtQBB7YxpgHGvt21U8YKdIRmGKo5wjk2Wj9xFwSGqlqm-SljkhjFFVn_NWqAWdJAr0428ckPbBTGjc7tt8yqZwlg88XJuhm7BNVeiipPQBW_kw.webp"
-}
-with st.expander(label=pokemon["name"],expanded=True):
-    st.image(pokemon["image_url"])
-    emoji_types=[
-        f"{type_emoji_dict[x]} {x}" for x in pokemon["types"]
-    ]
-    st.subheader(" / ".join(emoji_types))
+
+
+for i in range(0,len(pokemons),3):
+    row_poketmons=pokemons[i:i+3]
+    cols=st.columns(3)
+    for j in range(len(row_poketmons)):
+        with cols[j]:
+            pokemon=row_poketmons[j]
+            with st.expander(label=f"**{i+j+1}. {pokemon['name']}**",expanded=True):
+                st.image(pokemon["image_url"])
+                emoji_types=[
+                    f"{type_emoji_dict[x]} {x}" for x in pokemon["types"]
+                ]
+                st.subheader(" / ".join(emoji_types))
+
---
+++ config.py
@@ -0,0 +1,52 @@
+type_emoji_dict = {
+    "노말": "⚪",
+    "격투": "✊",
+    "비행": "🕊",
+    "독": "☠️",
+    "땅": "🌋",
+    "바위": "🪨",
+    "벌레": "🐛",
+    "고스트": "👻",
+    "강철": "🤖",
+    "불꽃": "🔥",
+    "물": "💧",
+    "풀": "🍃",
+    "전기": "⚡",
+    "에스퍼": "🔮",
+    "얼음": "❄️",
+    "드래곤": "🐲",
+    "악": "😈",
+    "페어리": "🧚"
+}
+pokemons = [
+    {
+        "name": "피카츄",
+        "types": ["전기"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/pikachu.webp"
+    },
+    {
+        "name": "누오",
+        "types": ["물", "땅"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/nuo.webp",
+    },
+    {
+        "name": "갸라도스",
+        "types": ["물", "비행"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/garados.webp",
+    },
+    {
+        "name": "개굴닌자",
+        "types": ["물", "악"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/frogninja.webp"
+    },
+    {
+        "name": "루카리오",
+        "types": ["격투", "강철"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/lukario.webp"
+    },
+    {
+        "name": "에이스번",
+        "types": ["불꽃"],
+        "image_url": "https://storage.googleapis.com/firstpenguine-coding-school/pokemons/acebun.webp"
+    },
+]
