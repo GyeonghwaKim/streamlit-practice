54ac492c782dd879ba5f9f27db27e769c720f7a6
GyeongHwa <gyeonghwaa_@naver.com>
2024-08-21 10:15:06 +0900

    5강 st.form

--- app.py
+++ app.py
@@ -10,6 +10,34 @@ st.title("streamlit 포켓몬 도감")
 st.markdown("**포켓몬**을 하나씩 추가해서 도감을 채워보세요")
 
 
+with st.form(key="form"):
+    col1,col2=st.columns(2)
+    with col1:
+        name=st.text_input(label="포켓몬 이름")
+    with col2:
+        types=st.multiselect(
+            label="포켓몬 속성",
+            options=list(type_emoji_dict.keys()),
+            max_selections=2
+        )
+    image_url=st.text_input(label="포켓몬 이미지 URL")
+    submit=st.form_submit_button(label="Submit")
+    if submit:
+        if not name:
+            st.error("포켓몬의 이름을 입려해주세요")
+        if len(types) ==0:
+            st.error("포켓몬의 속성을 적어도 한 개 설정해주세요")
+        else:
+            st.success("포켓몬을 추가할 수 있습니다")
+            pokemons.append(
+                {
+                    "name":name,
+                    "types":types,
+                    "image_url": image_url if image_url else "./images/default.png"
+                }
+            )
+
+
 
 for i in range(0,len(pokemons),3):
     row_poketmons=pokemons[i:i+3]
